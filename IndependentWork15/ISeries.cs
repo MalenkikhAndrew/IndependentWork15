a665293ace7a265112492c563602db8c9326e1b7
User <User@DESKTOP-A50L8A1>
2022-10-17 16:25:31 +1000

    Добавьте файлы проекта.

---
+++ IndependentWork15/ISeries.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndependentWork15
+{
+    interface ISeries
+    {
+        void setStart(int x);
+        int getNext();
+        void reset();
+    }
+    class ArithProgression : ISeries
+    {
+        int step;
+        int startValue;
+        int currentValue;
+        public int getNext() 
+        {
+            currentValue+=step;
+            return currentValue;
+        }
+        public void reset() 
+        {
+        currentValue = startValue;
+        }
+        public void setStart(int x) 
+        {
+        startValue=x;
+            currentValue = startValue;
+        }
+        public void setStep(int s) 
+        {
+        step=s;
+        }
+    }
+    class GeometricProgression : ISeries
+    {
+        int step;
+        int startValue;
+        int currentValue;
+        public int getNext()
+        {
+            currentValue*= step;
+            return currentValue;
+        }
+        public void reset()
+        {
+            currentValue = startValue;
+        }
+        public void setStart(int x)
+        {
+            startValue = x;
+            currentValue = startValue;
+        }
+        public void setStep(int s)
+        {
+            step = s;
+        }
+    }
+}
---
+++ IndependentWork15/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndependentWork15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Арифметическая прогрессия");
+            ArithProgression arith=new ArithProgression();
+            arith.setStart(5);
+            arith.setStep(3);
+            Console.WriteLine("Вывод первых 4 чисел");
+            Console.WriteLine(arith.getNext());
+            Console.WriteLine(arith.getNext());
+            Console.WriteLine(arith.getNext());
+            Console.WriteLine(arith.getNext());
+            arith.reset();
+            Console.WriteLine("Возврат к первоночальному значению и вывод 3 чисел");
+            Console.WriteLine(arith.getNext());
+            Console.WriteLine(arith.getNext());
+            Console.WriteLine(arith.getNext());
+            arith.reset();
+            Console.WriteLine("Геометрическая прогрессия");
+            GeometricProgression geometric = new GeometricProgression();
+            geometric.setStart(2);
+            geometric.setStep(2);
+            Console.WriteLine("Вывод первых 4 чисел");
+            Console.WriteLine(geometric.getNext());
+            Console.WriteLine(geometric.getNext());
+            Console.WriteLine(geometric.getNext());
+            Console.WriteLine(geometric.getNext());
+            geometric.reset();
+            Console.WriteLine("Возврат к первоночальному значению и вывод 3 чисел");
+            Console.WriteLine(geometric.getNext());
+            Console.WriteLine(geometric.getNext());
+            Console.WriteLine(geometric.getNext());
+            geometric.reset();
+            Console.ReadKey();
+        }
+    }
+}
